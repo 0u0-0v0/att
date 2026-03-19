e5f34c49d5cf4cfd00365631127f5f234e895745
0u0 <0u0an2@gmail.com>
2026-03-19 13:17:22 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
-const CACHE_NAME = 'attendance-v1';
+const CACHE_NAME = 'attendance-v3';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
-  // 항상 네트워크에서 최신 버전을 가져오도록 설정
   event.respondWith(fetch(event.request));
 });
